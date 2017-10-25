f5a5dd05edd30b674cb0b6c31e9a9517c1cda381
Tomas Milota <410113@mail.muni.cz>
2017-10-25 12:48:45 +0200

    JUnit v4.11, implementace metod v CarShopStorageService

--- git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
+++ git-mvn-seminar/car-shop/src/main/java/cz/fi/muni/carshop/services/CarShopStorageServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 import cz.fi.muni.carshop.CarShopStorage;
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
+import cz.fi.muni.carshop.exceptions.RequestedCarNotFoundException;
 
 public class CarShopStorageServiceImpl implements CarShopStorageService {
 
@@ -30,7 +31,20 @@ public class CarShopStorageServiceImpl implements CarShopStorageService {
 
 	@Override
 	public void addCarToStorage(Car car) {
+		if(car.getPrice() < 0) {
+			throw new IllegalArgumentException("Car price cannot be negative.");
+		}
 		CarShopStorage.getInstancce().getCars().computeIfAbsent(car.getType(), x -> new ArrayList<>()).add(car);
+	}
+
+	@Override
+	public void sellCar(Car car) throws RequestedCarNotFoundException {
+		Map<CarTypes, List<Car>> allCars = CarShopStorage.getInstancce().getCars();
+		if(!allCars.values().contains(car)) {
+			throw new RequestedCarNotFoundException("Requested car not found.");
+		}
+
+		allCars.values().remove(car);
 	}
 
 }
